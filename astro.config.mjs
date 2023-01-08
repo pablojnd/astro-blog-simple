f344efca3d3b836f7cd995dc58f79bcbd3f5b1fc
Pablo Narbona <pablodelvalle215@gmail.com>
2023-01-08 00:24:27 -0300

    antes de preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://example.com",
+  integrations: [],
+  server: { port: 8080, host: true },
+});
